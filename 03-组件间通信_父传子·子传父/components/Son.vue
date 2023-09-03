5ca55b92d225f99297d4fdef345da108baec5cbe
ltpbje <19836788252@163.com>
2023-09-03 17:33:51 +0800

    03-组件间通信_父传子·子传父完成

---
+++ 03-组件间通信_父传子·子传父/components/Son.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="son" style="border:3px solid #000;margin:10px">
+    <!-- 3.直接使用props的值 -->
+    我是Son组件 {{ title }}
+    <button @click="handelClick">修改标题</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Son-Child',
+  // 2.通过props来接受
+  props:['title'],
+  methods:{
+    // 子传父
+    // 1.$emit触发事件,给父组件发送消息通知
+    handelClick(){
+      this.$emit('changeTitle','传智教育')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
