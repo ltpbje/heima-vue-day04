b1778d772194ad4d257378d3880b04a6a4b46243
ltpbje <19836788252@163.com>
2023-09-08 11:02:12 +0800

    案例完成一部分

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- 主体区域 -->
+  <section id="app">
+    <TodoHeader @add="add"></TodoHeader>
+    <TodoMain :list="list" @del="handleDel"></TodoMain>
+    <TodoFooter></TodoFooter>
+  </section>
+</template>
+
+<script>
+import TodoFooter from './components/TodoFooter'
+import TodoHeader from './components/TodoHeader.vue'
+import TodoMain from './components/TodoMain.vue'
+export default {
+  data () {
+    return {
+      list:[
+        {id:1,name:'晨跑'},
+        {id:2,name:'吃饭'},
+        {id:3,name:'学习'}
+      ]
+    }
+  },
+  methods:{
+    add(todoName){
+      this.list.unshift({
+        id:+new Date(),
+        name:todoName
+      })
+    },
+    handleDel(id){
+    this.list = this.list.filter(item => item.id !== id)
+    }
+  },
+  components:{
+    TodoHeader,
+    TodoFooter,
+    TodoMain
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,30 @@
+<template>
+     <!-- 输入框 -->
+    <header class="header">
+        <h1>小黑记事本</h1>
+        <input placeholder="请输入任务" v-model="todoName" @keyup.enter="add" class="new-todo" />
+        <button class="add" @click="add">添加任务</button>
+    </header>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            todoName:''
+        }
+    },
+    methods:{
+        add(){
+            if(this.todoName.trim() ==''){
+                alert('输入不能为空')
+                return
+            }
+            this.$emit('add',this.todoName)
+            this.todoName = ''
+        }
+    }
+}
+</script>
+
+<style></style>
