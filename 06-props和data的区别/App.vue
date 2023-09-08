6fed584de4277cb73e9a42dc9fdad7f4f3f2370a
ltpbje <19836788252@163.com>
2023-09-08 09:41:59 +0800

    06-props和data的区别(自己的数据只能自己修改)练习完成

---
+++ 06-props和data的区别/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <BaseCount :count="count" @changeCount="changeCount"></BaseCount>
+  </div>
+</template>
+
+<script>
+import BaseCount from './components/BaseCount.vue'
+export default {
+  components:{
+    BaseCount
+  },
+  data(){
+    return {
+      count:100
+    }
+  },
+  methods:{
+    changeCount(count){
+      this.count = count
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 06-props和data的区别/components/BaseCount.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="base-count">
+    <button @click="countSub">-</button>
+    <span>{{ count }}</span>
+    <button @click="countAdd">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  // // 1.自己的数据随便修改  （谁的数据 谁负责）
+  // data () {
+  //   return {
+  //     count: 100,
+  //   }
+  // },
+  // 2.外部传过来的数据 不能随便修改
+    props:{
+        count:{
+          type:Number
+        }
+    },
+    methods:{
+      countSub(){
+          this.$emit('changeCount',this.count - 1)
+      },
+      countAdd(){
+          this.$emit('changeCount',this.count + 1)
+      }
+    }
+}
+</script>
+
+<style>
+.base-count {
+  margin: 20px;
+}
+</style>
