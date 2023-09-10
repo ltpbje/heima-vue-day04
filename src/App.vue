850ba97228e0a8df8a04e57852b68df61ad6d6a8
ltpbje <19836788252@163.com>
2023-09-10 17:05:31 +0800

    10-v-model的原理完成一部分

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <input v-model="msg1" type="text" />
+    <br />
+    <input :value="msg2" @input="msg2 = $event.target.value" type="text" >
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg1: '',
+      msg2: '',
+    }
+  },
+}
+</script>
+
+<style>
+</style>
