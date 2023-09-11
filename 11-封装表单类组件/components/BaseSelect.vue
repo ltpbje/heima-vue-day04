53f299a78222fb73edf0b646aae50925c8993bcd
ltpbje <19836788252@163.com>
2023-09-11 20:19:34 +0800

    11-封装表单类组件

---
+++ 11-封装表单类组件/components/BaseSelect.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <select :value="selectId" @change="changeId">
+      <option value="101">北京</option>
+      <option value="102">上海</option>
+      <option value="103">武汉</option>
+      <option value="104">广州</option>
+      <option value="105">深圳</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+      selectId:{
+        type:String
+      }
+    },
+    methods:{
+      changeId(e){
+        // console.log(e.target.value);
+        // 为该组件实例添加事件
+        this.$emit('changeId', e.target.value)
+      }
+    }
+}
+</script>
+
+<style>
+</style>
