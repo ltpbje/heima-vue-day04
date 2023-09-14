510d24048e4d1a812df89195d35b9db15482baf5
ltpbje <19836788252@163.com>
2023-09-14 20:58:58 +0800

    输入框v-model简化小作业完成

---
+++ 输入框v-model简化小作业/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <BaseInput v-model="inputValue"></BaseInput>
+  </div>
+</template>
+
+<script>
+import BaseInput from './components/BaseInput.vue'
+export default {
+  data(){
+    return{
+      inputValue:''
+    }
+  },
+  components:{
+    BaseInput
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 输入框v-model简化小作业/components/BaseInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text" :value="value" @input="handleChange">
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    value:{
+      type:String
+    }
+  },
+  methods:{
+    handleChange(e){
+      this.$emit('input',e.target.value)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
